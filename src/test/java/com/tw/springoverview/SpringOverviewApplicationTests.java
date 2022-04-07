eda00b1c7044746f00b9236931baff667cf3b7f1
zoubinthw <316324334@qq.com>
2022-04-07 21:57:46 +0800

    first create and connect

---
+++ src/test/java/com/tw/springoverview/SpringOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.springoverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOverviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
